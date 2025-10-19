8534b0c746efa58511e75e7a6bce4aa6c71b7b33
Sebastián <sebastianacaro6@gmail.com>
2025-10-18 23:04:56 -0500

    Semi conexión

---
+++ src/lib/api.ts
@@ -0,0 +1,8 @@
+const API = "http://localhost:4000";
+export async function api(path: string, opts: RequestInit = {}) {
+  const token = localStorage.getItem("token");
+  const headers = { "Content-Type":"application/json", ...(token? { Authorization:`Bearer ${token}` }:{}) };
+  const res = await fetch(API + path, { ...opts, headers });
+  if (!res.ok) throw new Error(await res.text());
+  return res.json();
+}
